0fb681dbe7995921dc6895ec33cf236334224b07
salemregisl-tech <salemregisl@gmail.com>
2026-09-22 14:30:16 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# URL du Webhook Discord (votre compte principal)
+URL_WEBHOOK_DISCORD = "https://discord.com"
+
+# URL d'accès à la plateforme de jeu
+URL_JEU_1WIN = "https://1win.pro"
+
+# Identifiant du serveur de flux pour l'interception réseau
+CIBLE_WS = "centrifugo-ws-mse.live.gamedev-tech.cc"
+
+# Seuil statistique : nombre minimal d'occurrences requises avant de valider une prédiction
+SEUIL_ECHANTILLON_MIN = 5
+
+# Pourcentage minimal de récurrence requis pour déclencher l'envoi d'une alerte
+SEUIL_PROBABILITE_ALERTE = 75.0
